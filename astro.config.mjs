e429afee9cbbfd18846b0824bda605b663f556bd
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-02-06 15:31:22 +0900

    feat: Add astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://portfolio.ryotanny.com",
+  integrations: [react(), image(), prefetch()]
 });

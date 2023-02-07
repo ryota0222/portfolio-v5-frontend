b7fe8ad24c14b7fbf1723135ee3717663437f5d0
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-02-07 20:58:02 +0900

    feat: Add IllustratorWork page

---
+++ src/features/IllustratorIntroduction/constants/index.ts
@@ -0,0 +1,64 @@
+import type { LineSticker } from '../types';
+
+export const LINE_STICKERS_LIST: LineSticker[] = [
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296958/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296959/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296978/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296966/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296963/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296977/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296976/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296975/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296970/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296968/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296974/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296965/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296972/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296971/android/sticker.png',
+  },
+  {
+    url: 'https://store.line.me/stickershop/product/22123088/ja',
+    image: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/564296969/android/sticker.png',
+  },
+];

9405233827b93e84941dae9d27f6f4ae7e144cee
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-02-07 16:22:21 +0900

    feat: Add EmbedBaseLinkCard component

---
+++ src/components/ui/EmbedBaseLinkCard/types/index.ts
@@ -0,0 +1,22 @@
+export interface EmbedBaseLinkCardProps {
+  /**
+   * タイトル
+   */
+  title: string;
+  /**
+   * 説明文
+   */
+  description: string;
+  /**
+   * URL
+   */
+  url: string;
+  /**
+   * ファビコン画像
+   */
+  favicon?: string;
+  /**
+   * サムネイル
+   */
+  thumbnail?: string;
+}

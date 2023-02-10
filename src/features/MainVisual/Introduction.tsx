e6e8b8ec020ef4aeef29d66a973d7321cdaa83ad
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-02-10 21:07:41 +0900

    feat: Optimize images

--- src/features/MainVisual/Introduction.tsx
+++ src/features/MainVisual/Introduction.tsx
@@ -4,9 +4,10 @@ export const Introduction = memo(() => {
   return (
     <div className="flex items-center">
       <img
-        src="/images/profile.JPG"
-        alt="RyoTa"
+        src="/images/profile-image.jpg"
+        alt="プロフィール画像"
         className="w-[64px] h-[64px] rounded-full mr-4 md:mr-8 md:w-[96px] md:h-[96px] lg:w-[120px] lg:h-[120px] object-cover"
+        loading="lazy"
       />
       <div>
         <span className="font-rokkitt font-bold text-black dark:text-white text-3xl md:text-5xl lg:text-6xl">

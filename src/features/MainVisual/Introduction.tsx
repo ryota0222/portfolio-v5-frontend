be2a146531288bde47402121bcb6fbb00d44f33b
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-02-10 17:52:35 +0900

    fix: Fix color contrust

--- src/features/MainVisual/Introduction.tsx
+++ src/features/MainVisual/Introduction.tsx
@@ -4,15 +4,15 @@ export const Introduction = memo(() => {
   return (
     <div className="flex items-center">
       <img
-        src="/images/profile.JPG"
+        src="/images/profile.jpg"
         alt="RyoTa"
-        className="w-[64px] h-[64px] rounded-full mr-4 md:mr-8 md:w-[96px] md:h-[96px] lg:w-[120px] lg:h-[120px]"
+        className="w-[64px] h-[64px] rounded-full mr-4 md:mr-8 md:w-[96px] md:h-[96px] lg:w-[120px] lg:h-[120px] object-cover"
       />
       <div>
         <span className="font-rokkitt font-bold text-black dark:text-white text-3xl md:text-5xl lg:text-6xl">
           I'm <span className="bg-clip-text text-transparent bg-gradation-clip bg-cover">RyoTa.</span>
         </span>
-        <div className="font-rokkitt text-zinc-400 font-bold text-md md:text-xl lg:text-2xl">
+        <div className="font-rokkitt text-zinc-500 dark:text-zinc-400 font-bold text-md md:text-xl lg:text-2xl">
           = Design Engineer + Illustrator
         </div>
       </div>

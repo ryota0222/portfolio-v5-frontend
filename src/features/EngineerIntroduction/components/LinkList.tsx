ef83be48680ce27e61703b15e5743838ae53b960
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-02-17 02:02:58 +0900

    perf: Change dl tag accessibility

--- src/features/EngineerIntroduction/components/LinkList.tsx
+++ src/features/EngineerIntroduction/components/LinkList.tsx
@@ -10,61 +10,63 @@ import { useIsDarkMode } from '@/components/ui/ModeToggleButton/hooks/useToggleM
 export const LinkList = memo(() => {
   const [isDarkMode] = useIsDarkMode();
   return (
-    <dl className="px-6 py-4 bg-zinc-50 dark:bg-zinc-800 flex flex-col">
-      <dt className="invisible h-0">リンク一覧</dt>
+    <>
       <span role="presentation" className="hidden">
         {isDarkMode ? 'dark' : 'light'}
       </span>
-      <div className="flex justify-evenly flex-wrap">
-        {/* Twitter */}
-        <dd aria-label="Twitter" title="Twitter">
-          <a href="https://twitter.com/RyoTa___0222" target="_blank" rel="noopener noreferrer">
-            <span role="img" aria-label="Twitter">
-              <Twitter color="#1DA1F2" />
-            </span>
-          </a>
-        </dd>
-        {/* GitHub */}
-        <dd aria-label="GitHub" title="GitHub">
-          <a href="https://github.com/ryota0222" target="_blank" rel="noopener noreferrer">
-            <span role="img" aria-label="GitHub">
-              <GitHub color={isDarkMode ? '#FFF' : '#222'} />
-            </span>
-          </a>
-        </dd>
-        {/* Qiita */}
-        <dd aria-label="Qiita" title="Qiita">
-          <a href="https://qiita.com/RyoTa_0222" target="_blank" rel="noopener noreferrer">
-            <span role="img" aria-label="Qiita">
-              <Qiita color={isDarkMode ? '#FFF' : '#000'} />
-            </span>
-          </a>
-        </dd>
-        {/* Zenn */}
-        <dd aria-label="Zenn" title="Zenn">
-          <a href="https://zenn.dev/ryota0222" target="_blank" rel="noopener noreferrer">
-            <span role="img" aria-label="Zenn">
-              <Zenn />
-            </span>
-          </a>
-        </dd>
-        {/* Note */}
-        <dd aria-label="Note" title="Note">
-          <a href="https://note.com/ryotanny" target="_blank" rel="noopener noreferrer">
-            <span role="img" aria-label="Note">
-              <Note color={isDarkMode ? '#FFF' : '#000'} />
-            </span>
-          </a>
-        </dd>
-        {/* つながる勉強会 */}
-        <dd aria-label="つながる勉強会" title="つながる勉強会">
-          <a href="https://www.notion.so/31469dcb91b245d1b5e1de8e0e4b3b4b" target="_blank" rel="noopener noreferrer">
-            <span role="img" aria-label="つながる勉強会">
-              <img src="/images/tsunagaru.png" alt="つながる勉強会" width={32} height={32} loading="lazy" />
-            </span>
-          </a>
-        </dd>
-      </div>
-    </dl>
+      <dl className="px-6 py-4 bg-zinc-50 dark:bg-zinc-800 flex flex-col">
+        <dt className="invisible h-0">リンク一覧</dt>
+        <div className="flex justify-evenly flex-wrap">
+          {/* Twitter */}
+          <dd aria-label="Twitter" title="Twitter">
+            <a href="https://twitter.com/RyoTa___0222" target="_blank" rel="noopener noreferrer">
+              <span role="img" aria-label="Twitter">
+                <Twitter color="#1DA1F2" />
+              </span>
+            </a>
+          </dd>
+          {/* GitHub */}
+          <dd aria-label="GitHub" title="GitHub">
+            <a href="https://github.com/ryota0222" target="_blank" rel="noopener noreferrer">
+              <span role="img" aria-label="GitHub">
+                <GitHub color={isDarkMode ? '#FFF' : '#222'} />
+              </span>
+            </a>
+          </dd>
+          {/* Qiita */}
+          <dd aria-label="Qiita" title="Qiita">
+            <a href="https://qiita.com/RyoTa_0222" target="_blank" rel="noopener noreferrer">
+              <span role="img" aria-label="Qiita">
+                <Qiita color={isDarkMode ? '#FFF' : '#000'} />
+              </span>
+            </a>
+          </dd>
+          {/* Zenn */}
+          <dd aria-label="Zenn" title="Zenn">
+            <a href="https://zenn.dev/ryota0222" target="_blank" rel="noopener noreferrer">
+              <span role="img" aria-label="Zenn">
+                <Zenn />
+              </span>
+            </a>
+          </dd>
+          {/* Note */}
+          <dd aria-label="Note" title="Note">
+            <a href="https://note.com/ryotanny" target="_blank" rel="noopener noreferrer">
+              <span role="img" aria-label="Note">
+                <Note color={isDarkMode ? '#FFF' : '#000'} />
+              </span>
+            </a>
+          </dd>
+          {/* つながる勉強会 */}
+          <dd aria-label="つながる勉強会" title="つながる勉強会">
+            <a href="https://www.notion.so/31469dcb91b245d1b5e1de8e0e4b3b4b" target="_blank" rel="noopener noreferrer">
+              <span role="img" aria-label="つながる勉強会">
+                <img src="/images/tsunagaru.png" alt="つながる勉強会" width={32} height={32} loading="lazy" />
+              </span>
+            </a>
+          </dd>
+        </div>
+      </dl>
+    </>
   );
 });

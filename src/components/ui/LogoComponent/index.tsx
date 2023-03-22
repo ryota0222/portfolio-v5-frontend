17ed3092ba6c1d69f4d2e354992cb748fde9f2a6
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-03-22 20:06:17 +0900

    fix: Responsive styles

--- src/components/ui/LogoComponent/index.tsx
+++ src/components/ui/LogoComponent/index.tsx
@@ -7,7 +7,7 @@ import { Logo } from '../icons/Logo';
 export const LogoComponent = memo(() => {
   const [isDarkMode] = useIsDarkMode();
   return (
-    <span className="bg-dark dark:bg-white inline-flex justify-center items-center p-2 rounded-full w-10 h-10">
+    <span className="bg-dark dark:bg-white inline-flex justify-center items-center p-2 rounded-full w-8 h-8 md:w-10 md:h-10">
       <>
         <span className="hidden">{isDarkMode ? 'dark' : 'light'}</span>
         <Logo color={isDarkMode ? '#000' : '#FFF'} />

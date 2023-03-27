145f19bc5903f5a3e44c419028ee0c477f4b8c5b
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-03-27 23:33:20 +0900

    feat: Add assets page

---
+++ src/features/Assets/components/AssetLinkCard.tsx
@@ -0,0 +1,34 @@
+import { memo } from 'react';
+
+import dayjs from '@/lib/dayjs';
+
+import type { AssetData } from '../types';
+
+export interface Props extends AssetData {}
+
+export const AssetLinkCard: React.FC<Props> = memo(({ title, content, url, date, favicon, thumbnail }) => {
+  return (
+    <a
+      href={url}
+      rel="noopener noreferrer"
+      target="_blank"
+      className="flex items-center w-full border dark:border-zinc-500 hover:bg-zinc-50 hover:dark:bg-zinc-800 rounded overflow-hidden"
+    >
+      <div className="p-4 flex-1 overflow-hidden">
+        <h1 className="font-bold text-sm md:text-md text-black dark:text-white">{title}</h1>
+        <div className="text-sm my-2 text-black dark:text-white line-clamp-2 opacity-80">
+          {dayjs(date).format('YYYY/MM/DD')}
+        </div>
+        <div className="overflow-hidden text-xs text-ellipsis whitespace-nowrap text-blue-500 dark:text-blue-300 underline">
+          <img src={favicon} className="mr-1 inline-block" alt="favicon" width={14} height={14} loading="lazy" />
+          {url}
+        </div>
+      </div>
+      {typeof thumbnail === 'string' && (
+        <div className="h-[90px] max-w-[180px] md:h-[120px] md:max-w-[230px] hidden md:block">
+          <img src={thumbnail} alt={title} className="w-full h-full" width={230} height={120} loading="lazy" />
+        </div>
+      )}
+    </a>
+  );
+});
---
+++ src/features/Assets/index.tsx
@@ -0,0 +1,66 @@
+import { Tab } from '@headlessui/react';
+import { memo, useEffect, useState } from 'react';
+
+import { AssetLinkCard } from './components/AssetLinkCard';
+import { useAssetData } from './hooks/useAssetData';
+
+import type { AssetDataList } from './types';
+
+const classNames = (...classes: string[]): string => {
+  return classes.filter(Boolean).join(' ');
+};
+
+const capitalize = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const Assets = memo(() => {
+  const data = useAssetData();
+  const [categories, setCategories] = useState<AssetDataList | null>(null);
+  useEffect(() => {
+    if (data !== null) {
+      setCategories({
+        all: [...data.note, ...data.zenn, ...data.zenn].sort((pre, next) => (pre.date > next.date ? -1 : 1)),
+        ...data,
+      });
+    }
+  }, [data]);
+  if (categories === null) return <></>;
+  return (
+    <>
+      <div className="w-full px-2 mx-auto">
+        <Tab.Group>
+          <Tab.List className="flex space-x-1 rounded-full bg-zinc-200/20 p-1 max-w-md mx-auto">
+            {Object.keys(categories).map((category) => (
+              <Tab
+                key={category}
+                className={({ selected }) =>
+                  classNames(
+                    'w-full rounded-full py-2 text-sm font-medium leading-5 text-white',
+                    'ring-opacity-0 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                    selected
+                      ? 'bg-dark dark:bg-white dark:text-dark shadow'
+                      : 'text-black dark:text-white hover:bg-zinc-400/[0.12] hover:text-zinc-80'
+                  )
+                }
+              >
+                {capitalize(category)}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels className="mt-8">
+            {Object.values(categories).map((assets, idx) => (
+              <Tab.Panel key={idx}>
+                <ul>
+                  {assets.map((asset, idx) => (
+                    <li key={idx} className="py-4">
+                      <AssetLinkCard {...asset} />
+                    </li>
+                  ))}
+                </ul>
+              </Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
+    </>
+  );
+});

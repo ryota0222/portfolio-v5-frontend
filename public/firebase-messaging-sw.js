5b60e18dfd613524206e728f32470ee1953c25b4
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-04-04 23:39:12 +0900

    feat: Register service worker

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAeYkWHt4TZj4IkijFNr0mKb5xwRa2B24k',
+  authDomain: 'portfolio-5483f.firebaseapp.com',
+  projectId: 'portfolio-5483f',
+  storageBucket: 'portfolio-5483f.appspot.com',
+  messagingSenderId: '87203010473',
+  appId: '1:87203010473:web:2ec64715afa1aba813d6b8',
+});
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+});

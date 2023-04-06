e99e3d337fb5b68e2392b488887a4c04fafb9444
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-04-06 11:55:17 +0900

    fix: Fix to call fetch-data when app come foreground from background

--- src/features/NewsNotification/hooks/useNewsData.ts
+++ src/features/NewsNotification/hooks/useNewsData.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import type { News, Response } from '../types';
 
@@ -7,21 +7,31 @@ type UseNewsData = () => News | null;
 
 export const useNewsData: UseNewsData = () => {
   const [data, setData] = useState<News | null>(null);
-  useEffect(() => {
-    let ignore = false;
-    void (async () => {
+  const fetchData = useCallback(async () => {
+    if (document.visibilityState === 'visible') {
       try {
         const endpoint = import.meta.env.PUBLIC_API_ENDPOINT;
         const response = await axios.get<Response>(`${endpoint}/news`);
         const data = response.data;
-        if (data.contents.length > 0 && !ignore) setData(data.contents[0]);
+        if (data.contents.length > 0) setData(data.contents[0]);
       } catch (err) {
         if (err instanceof Error) console.error(err.message);
       }
+    }
+  }, []);
+  useEffect(() => {
+    let ignore = false;
+    void (async () => {
+      !ignore && (await fetchData());
     })();
     return () => {
       ignore = true;
     };
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', fetchData);
+    return () => document.removeEventListener('visibilitychange', fetchData);
   }, []);
   return data;
 };
